ba38f21518838b13999820f4e0ea70eaffe82f2a
Katty <katerinafedorenk@mail.ru>
2017-01-08 23:43:36 +0300

    RESTful API angular

---
+++ netology-angular-pokemons-resource/app/src/PokemonList/PokemonListCtrl.js
@@ -0,0 +1,7 @@
+'use strict';
+
+pokemonApp.controller('PokemonListCtrl', function($scope, PokemonsService, BerryService) {
+
+    $scope.pokemons = PokemonsService.query();
+	$scope.berries = BerryService.query();
+});
